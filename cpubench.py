30215f9078ed499b9eb3d8b852f6e482b77a1025
Kirill <bikshanovk@gmail.com>
2020-04-19 16:48:01 +0300

    + script that can load all cpus on a server

--- cpubench.py
+++ cpubench.py
@@ -1,37 +1,27 @@
 # -*- coding: utf-8 -*-
-import random
-import time
-from threading import Thread
- 
- 
-class MyThread(Thread):
-    """
-    A threading example
-    """
-    
-    def __init__(self, name):
-        """Инициализация потока"""
-        Thread.__init__(self)
-        self.name = name
-    
-    def run(self):
-        """Запуск потока"""
-        #amount = random.randint(3, 15)
-        #time.sleep(amount)
-        msg = "%s is running" % self.name
-        print(msg)
-        while True:
-            j = 1/1 + 1*1 - 1
-    
-def create_threads():
-    """
-    Создаем группу потоков
-    """
-    for i in range(8):
-        name = "Thread #%s" % (i+1)
-        my_thread = MyThread(name)
-        my_thread.start()
- 
- 
-if __name__ == "__main__":
-    create_threads()
+import math
+import multiprocessing
+from multiprocessing import Process
+
+def cpuload(name):
+    print('hello', name)
+    while True:
+        j = 1/1 + 1*1 - 1
+
+if __name__ == '__main__':
+    if multiprocessing.cpu_count() == 1:
+        CPU_USED = 1
+    else:
+        CPU_USED=multiprocessing.cpu_count()/2
+        
+    print("Amount of processors on this server: ", multiprocessing.cpu_count())
+    print("How many processors do you want to load? Plese input number from 1 to ", multiprocessing.cpu_count())
+    for i in range(CPU_USED):
+        name = "Process #%s" % (i+1)
+        p = Process(target=cpuload, args=(name,))
+        p.start()
+
+#скрипт умеет нагружать работой все ядра процессора
+#По умолчанию нагружается половина процессоров в системе
+#Нужно научить при запуске скрипта выбирать процент загрузки процессора
+
