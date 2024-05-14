a332ec69c8b86dda9eea002bce44de37c7b02a35
Kirill Bikshanov <bikshanovk@gmail.com>
2024-05-14 13:59:27 +0400

    Review mounts.pl

--- mounts.pl
+++ mounts.pl
@@ -1,20 +1,24 @@
 #!/usr/bin/perl
- 
+
+use Config;
+
 $first = 1;
- 
-print "[\n";
- 
-for (`cat /proc/mounts`)
-{
-    ($fsname, $fstype) = m/\S+ (\S+) (\S+)/;
- 
-    print "\t,\n" if not $first;
-    $first = 0;
- 
-    print "\t{\n";
-    print "\t\t\"fsname\":\"$fsname\",\n";
-    print "\t\t\"fstype\":\"$fstype\"\n";
-    print "\t}\n";
+
+if ( "$Config{osname}" eq "linux" ) {
+    print "[\n";
+    for (`cat /proc/mounts`) {
+        
+        ($fsname, $fstype) = m/\S+ (\S+) (\S+)/;
+        
+        print "\t,\n" 
+        if not $first; $first = 0;
+    
+        print "\t{\n";
+        print "\t\t\"fsname\":\"$fsname\",\n";
+        print "\t\t\"fstype\":\"$fstype\"\n";
+        print "\t}\n";
+    }
+    print "]\n";
+} else {
+    print "\nerror : this script only works on linux\n"
 }
- 
-print "]\n";
